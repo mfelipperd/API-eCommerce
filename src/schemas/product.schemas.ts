752cf213bd5212d0321b50c5f3607e172b0d9a77
mfelipperd <felipperabelodurans@gmail.com>
2022-03-29 20:59:40 -0300

    corrige validations

--- src/schemas/product.schemas.ts
+++ src/schemas/product.schemas.ts
@@ -1,17 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const nameValidation = async (req: Request, res: Response, next: NextFunction) => {
-  const { name } = req.body;
-  if (!name) return res.status(400).json({ error: 'Name is required' });
-  if (typeof (name) !== 'string') res.status(422).json({ error: 'Name must be a string' });
-  if (name.length < 2) res.status(422).json({ error: 'Name must be longer than 2 characters' });
+  const data = req.body;
+  const nome = data.name;
+  if (!nome) return res.status(400).json({ error: 'Name is required' });
+  if (typeof (nome) !== 'string') return res.status(422).json({ error: 'Name must be a string' });
+  if (nome.length <= 2) {
+    return res.status(422).json({ error: 'Name must be longer than 2 characters' });
+  } 
   next();
 };
 
 export const amountValidation = async (req: Request, res: Response, next: NextFunction) => {
-  const { amount } = req.body;
-  if (!amount) return res.status(400).json({ error: 'Amount is required' });
-  if (typeof (amount) !== 'string') res.status(422).json({ error: 'Amount must be a string' });
-  if (amount.length < 2) res.status(422).json({ error: 'Amount must be longer than 2 characters' });
+  const data = req.body;
+  const p = data.amount;
+  if (!p) return res.status(400).json({ error: 'Amount is required' });
+  if (typeof (p) !== 'string') return res.status(422).json({ error: 'Amount must be a string' });
+  if (p.length <= 2) {
+    return res.status(422).json({ error: 'Amount must be longer than 2 characters' });
+  } 
   next();
 };
