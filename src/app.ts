28b24491971dc2d1d39ce57fc6d8253a52d56c6b
mfelipperd <felipperabelodurans@gmail.com>
2022-03-29 15:12:42 -0300

    cria endpoints

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.get('/products');
+app.post('/products');
+app.get('/orders');
+app.post('/users');
+
 app.use(express.json());
 
 export default app;
