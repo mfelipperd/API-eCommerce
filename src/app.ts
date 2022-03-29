4a2235af68cf83da295d06dcadb303e562d505c8
mfelipperd <felipperabelodurans@gmail.com>
2022-03-29 19:18:42 -0300

    altera rotas + validations

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import ProductController from './controller/productController';
+import { amountValidation, nameValidation } from './schemas/product.schemas';
 
 const app = express();
+const productController = new ProductController();
 
-app.get('/products');
-app.post('/products');
+app.get('/products', productController.getAllProdutcts);
+app.post('/products', nameValidation, amountValidation, );
 app.get('/orders');
 app.post('/users');
 
