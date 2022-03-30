16e2f072cd97f111518e39c1726b7d91bf5bb5c8
mfelipperd <felipperabelodurans@gmail.com>
2022-03-29 21:46:09 -0300

    altera app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,27 @@
 import express from 'express';
 import ProductController from './controller/productController';
 import { amountValidation, nameValidation } from './schemas/product.schemas';
+import { levelValidation, 
+  usernameValidation, classeValidation, passwordValidation } from './schemas/user.schemas';
+import UserController from './controller/user.controller';
 
 const app = express();
 const productController = new ProductController();
+const userController = new UserController();
 
 app.use(express.json());
 
 app.get('/products', productController.getAllProdutcts);
 app.post('/products', nameValidation, amountValidation, productController.createProduct);
 app.get('/orders');
-app.post('/users');
+app.post(
+  '/users', 
+  usernameValidation, 
+  classeValidation, 
+  levelValidation, 
+  passwordValidation,
+
+  userController.createUsers,
+);
 
 export default app;
